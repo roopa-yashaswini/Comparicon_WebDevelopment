9a900c9b071e42ea0e9ddce9ded85536ddb0eecd
Roopa Yashaswini <75267025+roopa-yashaswini@users.noreply.github.com>
2020-12-01 21:43:05 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	if(isset($_COOKIE['username'])){
-		setcookie("username", "", time()-1);
-		setcookie("pwd", "", time()-1);
-	}
-	session_destroy();
-	header('location: index.html');
-?>
